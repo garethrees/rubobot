3dc7655e6a43516c866a4b4f57f776f17eafdb32
Gareth Rees <gareth@garethrees.co.uk>
2019-09-15 11:07:45 +0100

    Generate rubobot gem

---
+++ lib/rubobot.rb
@@ -0,0 +1,6 @@
+require "rubobot/version"
+
+module Rubobot
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rubobot_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rubobot do
+  it "has a version number" do
+    expect(Rubobot::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
