992232aacca9e2bf42a15e033cbd55957f87691d
Gareth Rees <gareth@garethrees.co.uk>
2019-09-15 11:27:02 +0100

    Configure rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,9 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
+  # Disable monkey patching but allow global DSL
   config.disable_monkey_patching!
+  config.expose_dsl_globally = true
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
