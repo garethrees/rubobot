37bc658e48402906dd83e2e6c7a88c7b6100a62e
Gareth Rees <gareth@garethrees.co.uk>
2019-09-26 17:31:48 +0100

    Add configuration class
    Holds config for the tool

---
+++ lib/rubobot/configuration.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module RuboBot
+  # Configuration options
+  class Configuration
+    DEFAULTS = { verbose: false,
+                 commit_message: false,
+                 version: false }.freeze
+
+    def initialize(options = {})
+      @options = options
+    end
+
+    %i[verbose commit_message version].each do |option|
+      define_method("#{option}?") { parsed_options[option] }
+    end
+
+    private
+
+    def parsed_options
+      @parsed_options ||= DEFAULTS.merge(options)
+    end
+
+    attr_reader :options
+  end
+end
