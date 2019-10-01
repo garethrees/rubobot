7129368c7fdd7b392d4e4824ca021faee8acb8c0
Gareth Rees <gareth@garethrees.co.uk>
2019-10-01 20:20:24 +0100

    WIP v2
    Struggling with how to push an outputter all the way through

--- lib/rubobot/cli.rb
+++ lib/rubobot/cli.rb
@@ -6,12 +6,35 @@ module RuboBot
     STATUS_SUCCESS = 0
     STATUS_FAILURE = 1
 
-    def run(args = ARGV)
-      project_dir = ProjectDir.new(args[0] || Dir.pwd)
+    def self.run(argv)
+      cli_options = CliOptions.new(argv)
+      configuration = Configuration.new(cli_options)
+      project_dir = ProjectDir.new(cli_options[:path])
+      new(project_dir, configuration).run
+    end
+
+    def initialize(project_dir, configuration)
+      @project_dir = project_dir
+      @configuration = configuration
+    end
+
+    def run
+      return output(VERSION) if configuration.version?
+
       project_dir.run_rubobot
       STATUS_SUCCESS
     rescue RuboBot::RuboCop::NoOffensesError
       STATUS_FAILURE
     end
+
+    private
+
+    def output(string)
+      configuration.stdout.puts(string)
+      STATUS_SUCCESS
+    end
+
+    attr_reader :configuration
+    attr_reader :project_dir
   end
 end
