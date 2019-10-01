a6c4b214027f09926d8a0c46262fe0625ab51f80
Gareth Rees <gareth@garethrees.co.uk>
2019-10-01 20:20:24 +0100

    WIP v2
    Struggling with how to push an outputter all the way through

--- spec/rubobot/cli_spec.rb
+++ spec/rubobot/cli_spec.rb
@@ -1,8 +1,29 @@
 # frozen_string_literal: true
 
+require 'stringio'
+
 describe RuboBot::CLI do
+  let(:project_dir) { RuboBot::ProjectDir.new('/tmp') }
+
   describe '#run' do
-    subject { described_class.new.run }
-    it { is_expected.to eq(0) }
+    subject { described_class.new(project_dir, configuration).run }
+
+    let(:configuration) do
+      RuboBot::Configuration.new(version: true, stdout: StringIO.new)
+    end
+
+    context 'when version: true' do
+      it { is_expected.to eq(0) }
+    end
+
+    context 'when version: true and capturing stdout' do
+      let(:configuration) do
+        RuboBot::Configuration.new(version: true, stdout: $stdout)
+      end
+
+      it 'prints the version' do
+        expect { subject }.to output("#{RuboBot::VERSION}\n").to_stdout
+      end
+    end
   end
 end
